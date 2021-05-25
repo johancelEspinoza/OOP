040485a753a1033b2983d1d34145e22a7348500b
johancelEspinoza <84363312+johancelEspinoza@users.noreply.github.com>
2021-05-24 19:47:50 -0600

    Class Johancel created

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,13 +1,15 @@
 package com.ucreativa;
 
 import com.ucreativa.familia.johancel;
+import com.ucreativa.familia.eva;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
         johancel test = new johancel();
-
+        eva test1 = new eva();
 
     }
 }
---
+++ src/com/ucreativa/familia/eva.java
@@ -0,0 +1,10 @@
+package com.ucreativa.familia;
+
+public class eva {
+
+    public eva(){
+        System.out.println("Saludo");
+    }
+
+}//end
+
